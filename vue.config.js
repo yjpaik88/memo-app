f321a511509b421be7011dc72ef709560047790e
yjpaik88 <yjpaik88@naver.com>
2022-02-05 16:45:39 +0900

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'prodcution' ? '/memo-app/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/memo-app/' : '/',
     outputDir: 'docs'
 }
